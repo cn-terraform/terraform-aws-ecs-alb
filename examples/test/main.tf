134155bb6ade1cba986dffcd88c331ef09004c3a
Julian Nonino <noninojulian@gmail.com>
2020-05-05 15:05:52 +0100

    Removing provider block

---
+++ examples/test/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "base-network" {
+  source                                      = "cn-terraform/networking/aws"
+  version                                     = "2.0.7"
+  name_preffix                                = "base"
+  vpc_cidr_block                              = "192.168.0.0/16"
+  availability_zones                          = [ "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d" ]
+  public_subnets_cidrs_per_availability_zone  = [ "192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19" ]
+  private_subnets_cidrs_per_availability_zone = [ "192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19" ]
+}
+
+module "load_balancer" {
+  source          = "../../"
+  name_preffix    = "test-alb"
+  vpc_id          = module.networking.vpc_id
+  private_subnets = module.networking.private_subnets_ids
+  public_subnets  = module.networking.public_subnets_ids
+}
