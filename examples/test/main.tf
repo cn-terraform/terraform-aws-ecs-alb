a876ebb201a1fde650aed630948fc6429a54a595
Julian Nonino <noninojulian@gmail.com>
2020-09-04 14:21:18 +0100

    Fixing issues

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "base-network" {
   source                                      = "cn-terraform/networking/aws"
   version                                     = "2.0.8"
-  name_prefix                                = "base"
+  name_prefix                                 = "base"
   vpc_cidr_block                              = "192.168.0.0/16"
   availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
   public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
@@ -14,7 +14,7 @@ module "base-network" {
 
 module "load_balancer" {
   source          = "../../"
-  name_prefix    = "test-alb"
+  name_prefix     = "test-alb"
   vpc_id          = module.base-network.vpc_id
   private_subnets = module.base-network.private_subnets_ids
   public_subnets  = module.base-network.public_subnets_ids
