48dd4ada3e09b3eb68660d2022b44ff502f1832d
Julian Nonino <jnonino@users.noreply.github.com>
2025-06-23 23:12:35 +0100

    Update networking module (#71)

---
+++ examples/provide-own-bucket/main.tf
@@ -0,0 +1,61 @@
+module "base-network" {
+  source  = "cn-terraform/networking/aws"
+  version = "3.0.1"
+
+  cidr_block = "192.168.0.0/16"
+
+  public_subnets = {
+    first_public_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.0.0/19"
+    }
+    second_public_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.32.0/19"
+    }
+    third_public_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.64.0/19"
+    }
+    fourth_public_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.96.0/19"
+    }
+  }
+
+  private_subnets = {
+    first_private_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.128.0/19"
+    }
+    second_private_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.160.0/19"
+    }
+    third_private_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.192.0/19"
+    }
+    fourth_private_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.224.0/19"
+    }
+  }
+}
+
+locals {
+  bucket_id = "my-tf-test-bucket"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = local.bucket_id
+}
+
+module "load_balancer" {
+  source          = "../../"
+  name_prefix     = "test-alb"
+  vpc_id          = module.base-network.vpc_id
+  public_subnets  = [for subnet in module.base-network.public_subnets : subnet.id]
+  private_subnets = [for subnet in module.base-network.private_subnets : subnet.id]
+  log_bucket_id   = local.bucket_id
+}
---
+++ examples/provide-own-bucket/mock_provider.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+
+  endpoints {
+    apigateway     = "http://localstack:4566"
+    cloudformation = "http://localstack:4566"
+    cloudwatch     = "http://localstack:4566"
+    dynamodb       = "http://localstack:4566"
+    es             = "http://localstack:4566"
+    firehose       = "http://localstack:4566"
+    iam            = "http://localstack:4566"
+    kinesis        = "http://localstack:4566"
+    lambda         = "http://localstack:4566"
+    route53        = "http://localstack:4566"
+    redshift       = "http://localstack:4566"
+    s3             = "http://localstack:4566"
+    secretsmanager = "http://localstack:4566"
+    ses            = "http://localstack:4566"
+    sns            = "http://localstack:4566"
+    sqs            = "http://localstack:4566"
+    ssm            = "http://localstack:4566"
+    stepfunctions  = "http://localstack:4566"
+    sts            = "http://localstack:4566"
+  }
+}
