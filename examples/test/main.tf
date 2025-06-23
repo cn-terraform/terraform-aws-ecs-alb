995ccf0380dc37f1988a6f50dbbc36fe310fb03a
Julian Nonino <jnonino@users.noreply.github.com>
2025-06-23 23:01:32 +0100

    Update networking module

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -1,19 +1,54 @@
 module "base-network" {
-  source                                      = "cn-terraform/networking/aws"
-  version                                     = "2.0.13"
-  name_prefix                                 = "base"
-  vpc_cidr_block                              = "192.168.0.0/16"
-  availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+  source  = "cn-terraform/networking/aws"
+  version = "3.0.1"
+
+  cidr_block = "192.168.0.0/16"
+
+  public_subnets = {
+    first_public_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.0.0/19"
+    }
+    second_public_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.32.0/19"
+    }
+    third_public_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.64.0/19"
+    }
+    fourth_public_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.96.0/19"
+    }
+  }
+
+  private_subnets = {
+    first_private_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.128.0/19"
+    }
+    second_private_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.160.0/19"
+    }
+    third_private_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.192.0/19"
+    }
+    fourth_private_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.224.0/19"
+    }
+  }
 }
 
 module "load_balancer" {
   source          = "../../"
   name_prefix     = "test-alb"
   vpc_id          = module.base-network.vpc_id
-  private_subnets = module.base-network.private_subnets_ids
-  public_subnets  = module.base-network.public_subnets_ids
+  public_subnets  = [for subnet in module.base-network.public_subnets : subnet.id]
+  private_subnets = [for subnet in module.base-network.private_subnets : subnet.id]
   enable_s3_logs  = true
   log_bucket_id   = null
 }
